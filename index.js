9991275d652038d68756df5097e663313c71b16a
Ashkan Daie <ashkan@eqdsoft.com>
2017-08-16 12:29:34 -0700

    wrong platform in index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const dragDrop = require('bindings')('electron-dragdrop-win')
 const os = require('os');
 
 var dragDropExport;
-if(os.platform() == 'win') {
+
+if(os.platform() == 'win32') {
     dragDropExport = {
         doDragDrop: dragDrop.DragDrop
     }
